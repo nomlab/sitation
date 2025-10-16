75477e9dff9070a8f47bfa04989e8c1b2f65cef3
ShukiSasakura <sasakura2023@s.okayama-u.ac.jp>
2025-10-16 17:29:22 +0900

    Add function to remove unnecessary links

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
-use std::io::Cursor;
+use std::{collections::HashMap, io::Cursor};
 use html5ever::parse_document;
 use html5ever::tendril::TendrilSink;
 use markup5ever_rcdom::{Handle, NodeData, RcDom};
+use url::Url;
 
 // This is not proper HTML serialization, of course.
 
-fn walk(depth: usize, handle: &Handle) {
+fn get_links(depth: usize, handle: &Handle) -> Vec<String> {
     let node = handle;
+    let mut links: Vec<String> = vec![];
 
     if let NodeData::Element { ref attrs, ..} = node.data {
         for attr in attrs.borrow().iter() {
             if attr.name.local.to_string() == "href" || attr.name.local.to_string() == "src" {
-                println!("<{}>", attr.value);
-            } 
-        }   
+                links.push(attr.value.to_string());
+            }
+        }
     }
 
     for child in node.children.borrow().iter() {
-        walk(depth + 1, child);
+        let mut child_links = get_links(depth + 1, child);
+        links.append(&mut child_links);
+    };
+
+    links
+}
+
+fn is_descendant(base: &Url, target:&Url) -> bool {
+    let res = base.make_relative(target);
+    match res {
+        Some(rel_path) if rel_path.contains("..") => false,
+        Some(_rel_path) => true,
+        None => false,
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://gc.cs.okayama-u.ac.jp/lab/nom/")
+    let top_url = Url::parse("https://gc.cs.okayama-u.ac.jp/lab/nom/").unwrap();
+    let mut urls: HashMap<Url, bool> = HashMap::new();
+    let resp = reqwest::get(top_url.as_str())
         .await?
         .text()
         .await?;
@@ -34,7 +50,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .from_utf8()
         .read_from(&mut resp)
         .unwrap();
-    walk(0, &dom.document);
+
+    let links = get_links(0, &dom.document);
+    for link in links {
+        let url = top_url.join(link.as_str()).unwrap();
+
+        if !is_descendant(&top_url, &url) {
+            continue;
+        }
+
+        if let Some(_) = urls.insert(url.clone(), true){
+            println!("{url}(dup)");
+        }else {
+            println!("{url}");
+        };
+    }
 
     Ok(())
 }
